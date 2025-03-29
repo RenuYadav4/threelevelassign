2cfd242da95469405f6d0801ba50677e1634013a
Renu Yadav <renuya886@gmail.com>
2025-03-29 11:08:16 +0530

    changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,9 +7,9 @@ const Login = () => {
       <div className="relative bg-[#e1b998] h-4/5 w-[87%] md:p-[45px] lg:p-[55px] md:h-4/6 lg:h-6/7 lg:w-[80%] xl:[75%] shadow-2xl shadow-gray-500  rounded-2xl ">
 
         {/* 🔥 Custom Circle Shadow Effects */}
-        <div className="absolute -top-10 lg:-left-[-400px] h-20 w-20 md:-left-[-250px] md:w-30 md:h-30 lg:w-40 lg:h-40 bg-[#bba08a] opacity-50  rounded-full z-0"></div>
-        <div className="absolute top-1/2 -right-13 lg:-right-20 h-30 w-30 md:w-45 md:h-45 lg:w-50 lg:h-50 bg-[#bba08a] opacity-50  rounded-full z-0"></div>
-        <div className="absolute -bottom-12 right-60 w-42 h-42 bg-[#bba08a] opacity-50  rounded-full z-0"></div>
+        <div className="absolute -top-10 lg:-left-[-400px] h-20 w-20 md:-left-[-250px] md:w-30 md:h-30 lg:w-40 lg:h-40 bg-[#bba08a] opacity-50  rounded-full "></div>
+        <div className="absolute top-1/2 -right-13 lg:-right-20 h-30 w-30 md:w-45 md:h-45 lg:w-50 lg:h-50 bg-[#bba08a] opacity-50  rounded-full "></div>
+        <div className="absolute -bottom-12 right-60 w-42 h-42 bg-[#bba08a] opacity-50  rounded-full "></div>
 
         <div className='relative bg-[#51433d] rounded-2xl md:h-[95%] lg:h-[90%] md:w-[40%]  lg:w-3/6 xl:w-2/4 max-w-[400px] invisible sm:visible '>
           <p className='visible'>renu</p>
